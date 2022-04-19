04517f52c4f2f8300ca772b5d4e2572fa56ac6ae
AnuAja <101627379+AnuAja@users.noreply.github.com>
2022-04-19 16:37:42 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4271
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan karyawan = new karyawan(33090309, "Mutmainah", 2500000);
+            karyawan karyawan2 = new karyawan(33090390,  "Ramadhan", -3000000);
+            karyawan karyawan3 = new karyawan(33090949, "Mahmudin", 2000000);
+
+            Console.WriteLine("No\t Nik\t        Nama\t        GajiBulanan");
+            Console.WriteLine("===================================================");
+            karyawan.Karyawan();
+            karyawan2.Karyawan();
+            karyawan3.Karyawan();
+
+            Console.WriteLine("\nGAJIMU NAIK MASZEEH 10% !!\n");
+
+            Console.WriteLine("No\t Nik\t        Nama\t        GajiBulanan");
+            Console.WriteLine("===================================================");
+            karyawan.NaikGaji();
+            karyawan2.NaikGaji();
+            karyawan3.NaikGaji();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4271
+{
+    class karyawan
+    {
+        public int No { get; set; }
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public karyawan(int Nik, string Nama, int GajiBulanan)
+        {
+            this.Nik = Nik;
+            this.Nama = Nama;
+
+            if (GajiBulanan < 0)
+            {
+                this.GajiBulanan = 0;
+            }
+            else
+            {
+                this.GajiBulanan = GajiBulanan;
+            }
+        }
+        public void Karyawan()
+        {
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}", No, Nik, Nama, GajiBulanan);
+        }
+        public void NaikGaji()
+        {
+            double tmp = 0;
+            tmp = 0.1 * GajiBulanan;
+            GajiBulanan += Convert.ToInt32(tmp);
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}", No, Nik, Nama, GajiBulanan);
+        }
+    }
+}
